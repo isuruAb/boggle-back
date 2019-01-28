246c6797dd97b38a797ae5bace7ac773efd56103
isuruAb <vpisabeywardana@gmail.com>
2019-01-28 22:02:14 +0800

    send boggle board characters

--- app/api/v1/endpoints/word.rb
+++ app/api/v1/endpoints/word.rb
@@ -7,13 +7,27 @@ module V1
       prefix :api
       resource :word do
       
-        desc "Returns pong if logged in correctly"
+        desc "Returns Characters of boggle board"
         params do
             #requires :token, type: String, desc: "Access token."
         end
-        get :ping do   
-            puts 'done'
+        get :characters do   
+           # random= ['VSLHERAESITSMRPS']
+            {data: ['V','S','L','H','E','R','A','E','S','I','T','S','M','R','P','S']}
         end
+
+        desc "Check correctness of words"
+        params do
+            requires :word, type: String, desc: "Selected word."
+        end
+        post :selected do   
+
+           wordArr= ['SIT','IT','ARE','EAR'];
+           check=wordArr.include?(params[:word])
+            split=params[:word].split('')
+            {check:check,split:split}
+        end
+
       end
     end
   end
