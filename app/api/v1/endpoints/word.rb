44938dfdab025e41328b0a539504edd9c4f16ad2
isuruAb <vpisabeywardana@gmail.com>
2019-02-03 18:31:38 +0800

    check all selected combination with the dictionaries and send points

--- app/api/v1/endpoints/word.rb
+++ app/api/v1/endpoints/word.rb
@@ -3,7 +3,21 @@ require 'set'
 module V1
   module ENDPOINTS
     class Word < Grape::API
+      alphabet=['A','B','C','D','E','F','G','H','I','J','K','L','M',
+        'N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+      wordsWithStars=[]
+      # def getCombinations(startIndex, stars,iteration)
+      #   if(stars===0)
+      #     alphabet.each_with_index do |item, index|
+            
+      #     end
+      #     return wordsWithStars
+      #   else
+      #     for(c=iteration;c<alphabet.length;c++){
 
+      #     }
+      #   end
+      # end
       format :json
       prefix :api
       resource :boggle do
@@ -22,26 +36,38 @@ module V1
         end
         post :word do
           #Check whether there are any Stars (*)s 
-          indexesOfStar=[]
-        
-          params[:word].each_with_index do |char, index|
-            print ', * exists ', char==='*'
-            if char==='*'
-              indexesOfStar.push(index)
-            end
-          end
-
-          print "   ", indexesOfStar , '\n'
-
           word= params[:word].join('')
+          wordReg=word.downcase.gsub(/[*]/, "[a-z]")
+          regexObj=Regexp.new('\b'+wordReg+'\b')
+          puts "wordReg",wordReg
+
+          # Get List of words from the dictionary
           lines=[]
           File.open( "dictionary.txt" ) do |io|
             io.each {|line| line.chomp! ; lines << line}
           end
+
+
+          #create a regex object dynamically
           s=lines.to_set
-          checkResult= s.include?params[:word].join('').downcase 
-          puts 'result ',checkResult
-          {check:checkResult}
+          puts 'type od regexObj',regexObj
+          checkResult=lines.select { |e| regexObj =~ e }
+          puts "test select",checkResult            
+                      
+          # checkResult= lines.include?params[:word].join('').downcase 
+          points=0
+          hasWord=false
+
+          # Counting points
+          if(checkResult.length!=0)
+            hasWord=true
+            for x in 0..checkResult.length-1
+              points=points+1
+            end
+          end
+
+          print "points ",points
+          {check:hasWord,points:points}
         end
 
       end
